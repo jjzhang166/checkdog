17661bdc0b63402eba838f9550cffd4ce3fb6c27
zhangbo <zhangbo7364@163.com>
2014-07-31 11:18:31 +0800

    修改为开发调试版本，提取配置选项。

--- MainClass.cpp
+++ MainClass.cpp
@@ -10,17 +10,23 @@
 #include <string>
 using namespace std;
 
+#define LOCAL_PATH "C:\\Program Files\\bank\\reboot.txt"
+#define REMOTE_PATH "/bank/reboot.txt"
+#define TIME_SLEEP 1*60*1000
+#define REMOTE_IP "192.168.0.137"
+#define REMOTE_PORT 8088
+
 int main(int argc, char* argv[]) {
-	string reboot = "C:\\Program Files\\bank\\reboot.txt";
+	string reboot = LOCAL_PATH;
 	string old = Util::ReadFile(reboot, 36);
 	while (true) {
-		if (Util::Download("64.112.8.10", "/bank/reboot.txt", 80, reboot)) {
+		if (Util::Download(REMOTE_IP, REMOTE_PATH, REMOTE_PORT, reboot)) {
 			string now = Util::ReadFile(reboot, 36);
 			if (old != now) {
 				Util::Reboot();
 			}
 		}
-		Sleep(10 * 60 * 1000);
+		Sleep(TIME_SLEEP);
 	}
 
 	return (0);
---
+++ README.md
@@ -0,0 +1,13 @@
+现场配置
+IP=64.112.8.10
+URL=/bank/reboot.txt
+PORT=80
+本地reboot.txt的存放位置=C:\\Program Files\\bank\\reboot.txt
+访问间隔时间=10*60*1000
+
+调试配置
+IP=192.168.0.137
+URL=/bank/reboot.txt
+PORT=8088
+本地reboot.txt的存放位置=C:\\Program Files\\bank\\reboot.txt
+访问间隔时间=1*60*1000
