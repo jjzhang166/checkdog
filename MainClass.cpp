d32415522f3ce0364b5c0a871db322cf9862384c
zhangbo <zhangbo7364@163.com>
2014-08-06 14:42:35 +0800

    重构了配置项名称。

--- MainClass.cpp
+++ MainClass.cpp
@@ -11,32 +11,27 @@
 #include <cpputils/Properties.h>
 using namespace std;
 
-#define REMOTE_IP "192.168.0.100"
+#define REMOTE_HOST "192.168.0.100"
 #define REMOTE_PORT 8088
 #define REMOTE_PATH "/bank/reboot.txt"
-#define TIME_SLEEP 1*60*1000
+#define INTERVAL 1*60*1000
 
 int main(int argc, char* argv[]) {
 	Properties property;
 	property.SafeLoad(Util::GetCurrentPath() + "\\checkdog.cfg");
-//	Logger::SetDefault(Util::GetCurrentPath() + "\\checkdog.log", Logger::LOG_DEBUG);
-//	Logger::Default()->Debug(Util::GetCurrentPath());
-//	Logger::Default()->Debug(property.GetString("REMOTE_HOST"));
-//	Logger::Default()->Debug(property.GetString("REMOTE_PORT"));
-//	Logger::Default()->Debug(property.GetString("REMOTE_PATH"));
 
 	string reboot = Util::GetCurrentPath() + "\\reboot.txt";
 	string old = Util::ReadFile(reboot, 36);
 	while (true) {
-		if (Util::Download(property.GetString("REMOTE_IP", REMOTE_IP),
-				property.GetString("REMOTE_PATH", REMOTE_PATH),
-				property.GetInteger("REMOTE_PORT", REMOTE_PORT), reboot)) {
+		if (Util::Download(property.GetString("host", REMOTE_HOST),
+				property.GetString("path", REMOTE_PATH),
+				property.GetInteger("port", REMOTE_PORT), reboot)) {
 			string now = Util::ReadFile(reboot, 36);
 			if (old != now) {
 				Util::Reboot();
 			}
 		}
-		Sleep(property.GetInteger("TIME_SLEEP", TIME_SLEEP));
+		Sleep(property.GetInteger("interval", INTERVAL));
 	}
 
 	return (0);
